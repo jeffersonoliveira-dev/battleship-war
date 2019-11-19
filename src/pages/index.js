ca3f90c05f149e2a434845181c1296ac01542af3
Jefferson Oliveira <cardbeatt@gmail.com>
2019-11-19 17:27:45 -0300

    landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+
+// landing page
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
