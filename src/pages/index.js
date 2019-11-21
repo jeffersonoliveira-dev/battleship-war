49cc9ff584c8d3f0184d87c049cc82e0dd9582ef
Jefferson Oliveira <cardbeatt@gmail.com>
2019-11-21 18:47:26 +0000

    added global to page and created box for form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
+import styled from 'styled-components'
+import GlobalStyle from '../components/GlobalStyles'
 
 // landing page
 
+const Box = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  min-width: 600px;
+  min-height: 400px;
+  background-color: white;
+
+  @media only screen and (max-width: 768px) {
+    min-width: 80vw;
+  }
+`
+
+
 const IndexPage = () => (
   <Layout>
+    <Box>
+    </Box>
+    <GlobalStyle/>
   </Layout>
 )
 
