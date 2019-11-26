ca63d1ec0639298b9f70875bb9d9dff0cb82243a
Jefferson Oliveira <cardbeatt@gmail.com>
2019-11-26 13:21:38 +0000

    created api route

---
+++ server/routes/api/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+  console.log('you requested this api');
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/api/index');
 
 const app = express();
 
@@ -7,10 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/public/')));
 
 // An api endpoint that returns a short list of items
-app.get('/api/*', (req, res) => {
-  console.log('you requested this API');
-  // path here
-});
+app.use('/api', api);
 
 const port = process.env.PORT || 5000;
 app.listen(port);
