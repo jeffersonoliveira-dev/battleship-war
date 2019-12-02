76de02d65840b1117a58f1dce681e450d1d32826
Jefferson Oliveira <cardbeatt@gmail.com>
2019-12-02 13:26:20 +0000

    creating styled component and box to the landing game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
-import GlobalStyles from './components/GlobalStyles'
+import Form from './components/Form'
 
 
 export const Container = styled.div`
-height: 100%;
-width: 100%;
-position: relative;
-
+  height: 100%;
+  width: 100%;
+  position: relative;
+  background-color: blue;
 `;
+
+export const Title = styled.div`
+  width: 50%;
+  margin: 0 auto;
+  text-align: center;
+  position: relative;
+  top: 20px;
+`;
+
+
+
+export const Box = styled.div`
+  background-color: white;
+  height: 400px;
+  width: 50vw;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, 30%);
+
+
+  @media (max-width: 768px) {
+    width: 90vw;
+  }
+`;
+
 
 
 const App: React.FC = () => {
   return (
-    <div>
-       <h1>testing</h1>
-      <GlobalStyles/>
-     </div>
+    <Container>
+       <Title>Battleship War</Title>
+       <Box>
+        <Form/>
+       </Box>
+     </Container>
   );
 }
 
