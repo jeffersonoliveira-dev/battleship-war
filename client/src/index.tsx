1be6c718911045ec189f78c3fed269b910ecb512
Jefferson Oliveira <cardbeatt@gmail.com>
2019-12-16 14:46:16 +0000

    deleted service worker

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Home from "./routes/Home";
+import Game from "./routes/Game";
+import Score from "./routes/Score";
 import Dashboard from "./routes/Dashboard";
-import * as serviceWorker from "./serviceWorker";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import GlobalStyles from "./components/GlobalStyles";
 
@@ -12,13 +13,11 @@ ReactDOM.render(
     <div>
       <Route exact path="/" component={Home} />
       <Route path="/dashboard" component={Dashboard} />
+      <Route path="/game" component={Game} />
+      <Route path="/score" component={Score} />
       <GlobalStyles />
     </div>
   </Router>,
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
