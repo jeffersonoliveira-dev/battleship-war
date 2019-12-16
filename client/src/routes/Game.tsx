76f34ebfdd4a129997a7ecf99225aa010f4fc1f8
Jefferson Oliveira <cardbeatt@gmail.com>
2019-12-16 14:47:00 +0000

    created game and score routes/components

---
+++ client/src/routes/Game.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Game: React.FC = () => {
+  return (
+    <Container>
+      <h1>Game</h1>
+    </Container>
+  );
+};
+
+export default Game;
---
+++ client/src/routes/Score.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+const Score: React.FC = () => {
+  return (
+    <Container>
+      <h1>Score</h1>
+    </Container>
+  );
+};
+
+export default Score;
