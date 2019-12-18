9e769841eaf29725911a11504de0639954461246
Jefferson Oliveira <cardbeatt@gmail.com>
2019-12-18 01:09:25 +0000

    created score component

---
+++ client/src/components/Score.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+
+`
+
+const Score : React.FC = () => {
+  return (
+
+  )
+}
+
+
+export default Score
