7f8f11f556db04846389c3bcfaba33972338cf1e
Jefferson Oliveira <cardbeatt@gmail.com>
2019-12-24 13:24:44 +0000

    todo

--- client/src/routes/Game.tsx
+++ client/src/routes/Game.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import styled from "styled-components";
 
+// game start modal
+// player choice where to put all the ships
+// player can control the ships and rotate
+// a button to get ready to start game [ need to check if the player put all the ships on the board  ]
+// game init
+
 const Container = styled.div``;
 
 const Game: React.FC = () => {
